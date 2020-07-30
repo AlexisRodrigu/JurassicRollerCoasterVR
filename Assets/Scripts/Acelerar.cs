685a1219dca760265c679be886683899b73edb3c
Alexis Rodriguez <alexis.rodriguez94@hotmail.com>
2020-07-29 23:39:55 -0500

    Cambio de texturas y modelos 3D borrados
    Se borraron modelos 3D que sobraban en el proyecto y cambiaron texturas

---
+++ Assets/Scripts/Acelerar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acelerar : MonoBehaviour
+{
+    public PowerUp powerUpScript;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            powerUpScript.Acelerar();
+        }
+    }
+}
